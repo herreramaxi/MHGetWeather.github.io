4d64e013d183d0febe556d00a2facce9ad054ab2
Maxi <herreramaxi@gmail.com>
2020-01-11 13:10:31 +0000

    testing Oauth on yahoo api

--- weather.js
+++ weather.js
@@ -32,7 +32,58 @@ function InfoWeather(latitude, longitude) {
 
     buildQuery(this);
 
-    var getWeather = function (infoWeather) {
+	var getWeather = function (infoWeather) {
+		//Testing oauth on yahoo API
+			
+	var url = 'https://weather-ydn-yql.media.yahoo.com/forecastrss';
+	var method = 'GET';
+	var app_id = 'anJvMP76';
+	var consumer_key = 'dj0yJmk9ZkpyeTg4aDJ0N2RNJmQ9WVdrOVlXNUtkazFRTnpZbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTNh';
+	var consumer_secret = 'b418ae1c47ae6fcc8840bab13f0e566cf568b5ed';
+	var concat = '&';
+	var query = {'location': 'sunnyvale,ca', 'format': 'json'};
+	var oauth = {
+		'oauth_consumer_key': consumer_key,
+		'oauth_nonce': Math.random().toString(36).substring(2),
+		'oauth_signature_method': 'HMAC-SHA1',
+		'oauth_timestamp': parseInt(new Date().getTime() / 1000).toString(),
+		'oauth_version': '1.0'
+	};
+
+			var merged = {}; 
+
+			$.extend(merged, query, oauth);
+			// Note the sorting here is required
+			var merged_arr = Object.keys(merged).sort().map(function(k) {
+			  return [k + '=' + encodeURIComponent(merged[k])];
+			});
+			var signature_base_str = method
+			  + concat + encodeURIComponent(url)
+			  + concat + encodeURIComponent(merged_arr.join(concat));
+
+			var composite_key = encodeURIComponent(consumer_secret) + concat;
+			var hash = CryptoJS.HmacSHA1(signature_base_str, composite_key);
+			var signature = hash.toString(CryptoJS.enc.Base64);
+
+			oauth['oauth_signature'] = signature;
+			var auth_header = 'OAuth ' + Object.keys(oauth).map(function(k) {
+			  return [k + '="' + oauth[k] + '"'];
+			}).join(',');
+
+			$.ajax({
+			  url: url + '?' + $.param(query),
+			  headers: {
+				'Authorization': auth_header,
+				'X-Yahoo-App-Id': app_id 
+			  },
+			  method: 'GET',
+			  success: function(data){
+				console.log(data);
+			  }
+			}); 
+}
+	
+    var getWeather2 = function (infoWeather) {
         return $.ajax({
             url: infoWeather.url,
             jsonp: "callback",
